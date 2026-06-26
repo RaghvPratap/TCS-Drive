a5103734597f73d00795c806862532ad8680174f
Raghvendra Pratap Singh <114940855+RaghvPratap@users.noreply.github.com>
2026-06-26 14:33:23 +0530

    Update leapYear.java

--- leapYear.java
+++ leapYear.java
@@ -1,1 +1,11 @@
-
+class Solution{
+  public boolean check(int yr){
+    if((yr%4==0 && yr%100!=0) || (yr%400==0))return true;
+    else return false;
+  }
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    return check(n);
+  }
+}
